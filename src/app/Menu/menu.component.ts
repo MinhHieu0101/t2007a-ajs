2c0aa7bdcaf562f73068a381419c9b8f74640ee5
MinhHieu0101 <tung198q@gmail.com>
2022-10-14 16:01:29 +0700

    exam

--- src/app/Menu/menu.component.ts
+++ src/app/Menu/menu.component.ts
@@ -1,8 +1,37 @@
 import {Component} from "@angular/core";
+import {HttpClient} from "@angular/common/http";
+import * as url from "url";
 @Component({
   selector:'menu',
   templateUrl:'./menu.component.html'
 })
-export class MenuComponent{
+export class MenuComponent {
+  menu: any = [
+    {id: 1, name: 'Chicken', icon: 'abc'},
+    {id: 2, name: 'Rice', icon: 'abc'},
+  ];
+  flag=true;
+  on(){
+    this.flag=true;
+  }
+  off(){
+    this.flag=false;
+  }
+  constructor(private http: HttpClient) {
+  }
 
+  ngOnInit() {
+    this.getmenu();
+  }
+
+  getmenu() {
+    const url = 'https://foodgroup.herokuapp.com/api/menu'
+    this.http.get<any>(url)
+      .subscribe(data => {
+        var ls = data.data;
+        this.menu = ls;
+      });
+  }
 }
+
+
